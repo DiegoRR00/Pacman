4523d593fcd8b0a93b2f2d0552d067077be9253a
DiegoRR00 <reyna.diego.r00@gmail.com>
2020-10-28 14:22:50 -0600

    Cambio a velocidad fantasma y fantasmas inteligentes

--- pacman.py
+++ pacman.py
@@ -1,4 +1,4 @@
-from random import choice, randrange
+from random import choice, randrange, randint
 from turtle import *
 from freegames import floor, vector
 
@@ -7,13 +7,24 @@ path = Turtle(visible=False)
 writer = Turtle(visible=False)
 aim = vector(5, 0)
 pacman = vector(-40, -80)
-ghostSpeed = randrange(3,11)
+step=5#Cambio en un solo paso
+velocidad=randint(1,2)#Selecciona aleatoriamiente la velocidad del fantasma
+ghostSpeed = step*velocidad#Define la velocidad del fantasma
+#Declarar los fantasma
 ghosts = [
     [vector(-180, 160), vector(ghostSpeed, 0)],
     [vector(-180, -160), vector(0, ghostSpeed)],
     [vector(100, 160), vector(0, -ghostSpeed)],
     [vector(100, -160), vector(-ghostSpeed, 0)],
 ]
+#Posibles movimientos del fantasma
+options = [
+    vector(ghostSpeed, 0),
+    vector(-ghostSpeed, 0),
+    vector(0, ghostSpeed),
+    vector(0, -ghostSpeed),
+]
+#Dos posibles tableros
 allTiles = [[
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
@@ -57,8 +68,8 @@ allTiles = [[
     0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 ]]
-tiles = allTiles[randrange(len(allTiles))]
-
+tiles = allTiles[randrange(len(allTiles))]#Seleccionar uno de los dos tableros
+#Dibuar los cuadros del tablero
 def square(x, y):
     "Draw square using path at (x, y)."
     path.up()
@@ -71,14 +82,14 @@ def square(x, y):
         path.left(90)
 
     path.end_fill()
-
+#Regresa la posición en el array del tablero
 def offset(point):
     "Return offset of point in tiles."
     x = (floor(point.x, 20) + 200) / 20
     y = (180 - floor(point.y, 20)) / 20
     index = int(x + y * 20)
     return index
-
+#Regresa si el movimiento es válido
 def valid(point):
     "Return True if point is valid in tiles."
     index = offset(point)
@@ -92,7 +103,7 @@ def valid(point):
         return False
 
     return point.x % 20 == 0 or point.y % 20 == 0
-
+#Dibuja el mapa
 def world():
     "Draw world using path."
     bgcolor('black')
@@ -110,7 +121,7 @@ def world():
                 path.up()
                 path.goto(x + 10, y + 10)
                 path.dot(4, 'white')
-
+#Mueva al pacman y a los fantasmas
 def move():
     "Move pacman and all ghosts."
     writer.undo()
@@ -118,11 +129,11 @@ def move():
 
     clear()
 
-    if valid(pacman + aim):
+    if valid(pacman + aim):#Mover al pacman si se puede mover
         pacman.move(aim)
 
     index = offset(pacman)
-
+    #Actualiza el score
     if tiles[index] == 1:
         tiles[index] = 2
         state['score'] += 1
@@ -133,17 +144,13 @@ def move():
     up()
     goto(pacman.x + 10, pacman.y + 10)
     dot(20, 'yellow')
-
-    for point, course in ghosts:
-        if valid(point + course):
+    index=0
+    for point, course in ghosts:#Ver como se mueven los fantasmas
+        searchPacman(index)#Checar si el pacman está en campo de visión
+        index+=1#Mantener el número de fantasmas
+        if valid(point + course):#Si es posible el movimiento deseado
             point.move(course)
-        else:
-            options = [
-                vector(ghostSpeed, 0),
-                vector(-ghostSpeed, 0),
-                vector(0, ghostSpeed),
-                vector(0, -ghostSpeed),
-            ]
+        else:#Si no lo es, intentar una nueva dirección
             plan = choice(options)
             course.x = plan.x
             course.y = plan.y
@@ -153,25 +160,64 @@ def move():
         dot(20, 'red')
 
     update()
-
+    #Checar si se acabó
     for point, course in ghosts:
         if abs(pacman - point) < 20:
             return
 
     ontimer(move, 50)
-
+#Define si hay un pacman en el campo de visión del fantasma
+def searchPacman(index):
+    if (pacman.y in range(ghosts[index][0].y-ghostSpeed,ghosts[index][0].y+ghostSpeed)):#Si están alineados en y. Es rango pq el paso de un fantasma es más grande que el de pacman
+        #Evaluar un camino en x
+        biggest=ghosts[index][0].x
+        smallest=pacman.x
+        temp=vector(0,pacman.y)#Vector para checar el camino
+        direccion=1#Indice del movimiento en x
+        if (biggest<smallest):#Si biggest y smallest fueron declarados mal
+            smallest=biggest
+            biggest=pacman.x
+            direccion=0
+        temp.x=smallest#Actualizar temp
+        while(temp.x!=biggest):#Mientras no se llegue al objetivo
+            temp+=options[direccion]#Mover temp
+            if (not(valid(temp))):#Si choca con pared, terminar evaluación
+                return 
+        #Cambiar la dirección del fantasma
+        ghosts[index][1].x=options[direccion].x
+        ghosts[index][1].y=options[direccion].y
+    elif (pacman.x in range(ghosts[index][0].x-ghostSpeed,ghosts[index][0].x+ghostSpeed)):#Si están alineados en x. Es rango pq el paso de un fantasma es más grande que el de pacman
+        #Evaluar camino en y
+        biggest=ghosts[index][0].y
+        smallest=pacman.y
+        temp=vector(pacman.x,0)#Vector para checar el camino
+        direccion=3#Indice del movimiento en y
+        if (biggest<smallest):#Si biggest y smallest fueron declarados mal
+            smallest=biggest
+            biggest=pacman.y
+            direccion=2
+        temp.y=smallest#Actualizar temp
+        while(temp.y!=biggest):#Mientras no se llegue al objetivo
+            temp+=options[direccion]#Mover temp
+            if (not(valid(temp))):#Si choca con pared, terminar evaluación
+                return 
+        #Cambiar la dirección del fantasma
+        ghosts[index][1].x=options[direccion].x
+        ghosts[index][1].y=options[direccion].y
+#Cambia la posición del pacman
 def change(x, y):
     "Change pacman aim if valid."
     if valid(pacman + vector(x, y)):
         aim.x = x
         aim.y = y
 
-setup(420, 420, 370, 0)
+setup(420, 420, 370, 0)#Crear ventana
 hideturtle()
 tracer(False)
 writer.goto(160, 160)
 writer.color('white')
 writer.write(state['score'])
+#Guardar entradas del teclado
 listen()
 onkey(lambda: change(5, 0), 'Right')
 onkey(lambda: change(-5, 0), 'Left')
@@ -179,4 +225,4 @@ onkey(lambda: change(0, 5), 'Up')
 onkey(lambda: change(0, -5), 'Down')
 world()
 move()
-done()
+done()#Terminar
